08cddb50ed3918b6b55bf9ba12ab9e806e7efe01
Turko68 <6045909@mborijnland.nl>
2025-03-25 11:04:01 +0100

    Website Added

---
+++ footer.php
@@ -0,0 +1,29 @@
+
+
+<footer>
+    <section class="footer-content">
+        <section class="logo">
+            <img src="images/flogo.png" alt="Umami Sushi Logo">
+        </section>
+        <section class="right-content">
+            <section class="slogan">
+                <h4>HERE YOU FIND</h4>
+            </section>
+
+            <section class="social-media">
+                <a href="https://www.instagram.com/" target="_blank" aria-label="Instagram">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://www.facebook.com/" target="_blank" aria-label="Facebook">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="https://x.com/?lang=en" target="_blank" aria-label="Twitter">
+                    <i class="fab fa-twitter"></i>
+                </a>
+            </section>
+        </section>
+    </section>
+    <section class="copyright">
+        <p>&copy; 2024 Your Website. All rights reserved. Privacy Policy Terms of Service</p>
+    </section>
+</footer>
